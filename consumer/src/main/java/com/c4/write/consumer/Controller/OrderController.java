fc85ccaf3acfd649a578957e6e6d69b338d227d4
tfji <1510266928@qq.com>
2018-08-02 17:46:20 +0800

    消费者测试

---
+++ consumer/src/main/java/com/c4/write/consumer/Controller/OrderController.java
@@ -0,0 +1,20 @@
+package com.c4.write.consumer.Controller;
+
+import com.c4.write.consumer.bean.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import com.c4.write.consumer.service.OrderService;
+
+@RestController
+public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping(value = "order/{orderId}")
+    public Order queryOrderById(@PathVariable("orderId") String orderId) {
+        return orderService.queryOrderById(orderId);
+    }
+
+}
